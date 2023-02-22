8a6221d68867b999f50f4841cebe0e173a83eaec
Namsophia <namsophie9@gmail.com>
2023-02-22 11:16:53 -0800

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,11 +7,13 @@
  */
 int print_last_digit(int n)
 {
-	int ld = n % 10;
+	int ld;
 
+	ld = n % 10;
 	if (ld < 0)
-		ld *= -1;
-
+	{
+	ld = ld * -1;
+	}
 	_putchar(ld + '0');
-	return (0);
+	return (ld);
 }
