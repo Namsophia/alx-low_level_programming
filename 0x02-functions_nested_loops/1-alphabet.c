2fc90f4c3c7184c3cff9d0bc7db9037177127244
Namsophia <namsophie9@gmail.com>
2023-02-21 17:05:37 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alpha in lowercase
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	_putchar(la);
+
+	_putchar('\n');
+}
