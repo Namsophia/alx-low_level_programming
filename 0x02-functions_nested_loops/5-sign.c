c8a49d406559af74159f363e902f3fade47ac862
Namsophia <namsophie9@gmail.com>
2023-02-22 10:18:38 -0800

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,7 +11,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchat('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
@@ -24,3 +24,4 @@ int print_sign(int n)
 		_putchar('-');
 		return (-1);
 	}
+}
