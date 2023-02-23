9795fa2f50db719c0ffb06f7336039eb0b82d990
Namsophia <namsophie9@gmail.com>
2023-02-22 16:26:09 -0800

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,6 +14,7 @@ void times_table(void)
 	{
 		_putchar('0');
 		for (mult = 1; mult <= 9; mult++)
+
 		{
 			_putchar(',');
 			_putchar(' ');
@@ -22,7 +23,7 @@ void times_table(void)
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) + '0')
+				_putchar((prod / 10) + '0');
 			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
