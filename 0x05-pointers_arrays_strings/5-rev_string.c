5a40b0da188fe862dbd18c6802d337bca22a7211
Namsophia <namsophie9@gmail.com>
2023-02-27 13:29:07 -0800

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rev_string - function that reverses a string
+ * @s: String to reverse
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int aux = 0;
+
+	char 1temp;
+
+	while (*(s + i) != '\0')
+		i += 1;
+	i -= 1;
+
+	while (aux < i)
+	{
+		1temp = s[i];
+		s[i] = s[aux];
+		s[aux] = 1temp;
+		aux++;
+		i--;
+	}
+}
