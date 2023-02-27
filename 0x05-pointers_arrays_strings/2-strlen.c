cb0d8035eb1ce03f963f2e720d7a672ff65eb0da
Namsophia <namsophie9@gmail.com>
2023-02-27 12:13:32 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * strlen - a function that returns the length of a string
+ * @s: pointer to the string
+ * Return: void
+ */
+int strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	i++;
+
+	return (i);
+}
