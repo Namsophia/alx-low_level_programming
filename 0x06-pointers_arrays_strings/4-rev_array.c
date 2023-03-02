f7c6780de811abaeac5df9ae35a64d1c67dd1170
Namsophia <namsophie9@gmail.com>
2023-03-02 11:54:15 -0800

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_array - reverse array of integers
+ * @a: array
+ * @n: number of elements of array
+ * Retur: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int t;
+
+	for (i = 0; i < n--; i++)
+	{
+		t = a[i];
+		a[1] = a[n];
+		a[n] = t;
+	}
+}
