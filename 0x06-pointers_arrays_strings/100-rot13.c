27fe3d66d78ff1b5cb6f3b92fb7ac51feb227215
Namsophia <namsophie9@gmail.com>
2023-03-03 17:07:27 -0800

    task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - Encodes a string using rot13
+ * @str: The string to be encoded
+ * Return: A pointer to the encoded string
+ */
+char *rot13(char *str)
+{
+	int i;
+	int j;
+	char data[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdef
+ghijklmnopqrstuvwxyz";
+	char datarot[] = NOPQRSTUVWXYZABCDEFGHIJKLMnopqr
+stuvwxyzabcdefghijklm";
+	for (i = 0; s [i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == data1[j])
+			{
+				s[i] = datarot[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
