26e538b4d0c4372a4320d64c7eaca1d0e34607e8
Namsophia <namsophie9@gmail.com>
2023-03-06 15:51:55 -0800

    task 6

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,15 +4,5 @@
  * @a: array
  * Return: Always
  */
-void _strstr(char (*a)[8])
-{
-	int h;
-	int i;
-
-	for (h = 0; h < 8; h++)
-	{
-		for (i = 0; i < 8; i++)
-			_putchar(a[h][i]);
-		_putchar('\n');
-	}
+void
 }
