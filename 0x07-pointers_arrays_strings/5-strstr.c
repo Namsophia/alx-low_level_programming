ecda8a01a1de5689dfd2ed541efb55abe7684cef
Namsophia <namsophie9@gmail.com>
2023-03-06 17:15:31 -0800

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,8 +1,24 @@
 #include "main.h"
 /**
  * _strstr - locates a substring
- * @a: array
+ * @haystack: input
+ * @needle: input
  * Return: Always
  */
-void
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char * 1 = haystack;
+		char *p = needle;
+
+		while (*1 == *p && *p != '\0')
+		{
+			1++;
+			p++;
+		}
+		if (*p == '\0')
+			return (haystack);
+	}
+	return (0);
 }
