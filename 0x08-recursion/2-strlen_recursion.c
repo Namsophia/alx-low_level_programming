f003d622bfaf5b09868f7536d8035fa2bc9bf804
Namsophia <namsophie9@gmail.com>
2023-03-09 01:28:54 -0800

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to be returned
+ *
+ * Return: the length
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+	return (len);
+}
