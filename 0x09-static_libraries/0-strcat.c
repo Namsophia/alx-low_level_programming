420c376540c8e0752c133f9277a8552b36ef0fb4
Namsophia <namsophie9@gmail.com>
2023-03-10 16:02:51 -0800

    libraries

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,30 +1,23 @@
+
 #include "main.h"
- /**
- * _strcat - concatenates two strings
- * @dest: input value
- * @src: input value
- *
- * Return: void
+
+/**
+ * *_strcat - concatenates two strings
+ * @src: string to be appended to @dest
+ * @dest: string to append to.
+ * Return: Appended string
  */
+
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int index = 0;
+	int length = 0;
 
+	for (index = 0; dest[index]; index++)
+		length++; /* Getting length of desk to determine append start point */
 
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
+	for (index = 0; src[index]; index++, length++)
+		dest[length] = src[index];
 
-	dest[i] = '\0';
 	return (dest);
 }
