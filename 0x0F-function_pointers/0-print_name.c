e0aea5a24524e1f9de7bec2bc5e126fd255bb059
Namsophia <namsophie9@gmail.com>
2023-03-22 14:55:09 -0700

    prints name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,10 +5,11 @@
  * print_name - function that prints a name
  * @name: name to print
  * @f: pointer to the printing function
+ * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
+	if (name == NULL || f == NULL)
 		return;
 	f(name);
 }
