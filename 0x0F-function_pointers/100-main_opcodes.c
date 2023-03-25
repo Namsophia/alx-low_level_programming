f021fca686f4496a1cfe5e6a5dbbde66fc758e9b
Namsophia <namsophie9@gmail.com>
2023-03-24 18:12:41 -0700

    op codes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -3,10 +3,10 @@
 
 /**
  * main - prints its own opcodes
- * @argc: number of aguments
+ * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
@@ -19,6 +19,7 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 	bytes = atoi(argv[1]);
+
 	if (bytes < 0)
 	{
 		printf("Error\n");
