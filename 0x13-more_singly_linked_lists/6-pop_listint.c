629f5f7242e1208b3a0489b1d5343024e16b40c5
Namsophia <namsophie9@gmail.com>
2023-04-04 15:58:06 -0700

    pop listint

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -13,7 +13,8 @@ int pop_listint(listint_t **head)
 
 	if (!head || !*head)
 		return (0);
-	num = (*head)->next;
+	num = (*head)->n;
+	temp = (*head)->next;
 	free(*head);
 	*head = temp;
 	return (num);
