8af1ba11568317375eba0d0f01e4265fc5bbad44
Namsophia <namsophia9@gmail.com>
2023-06-09 01:46:20 -0700

    1-print

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _rev_recursion - prints string in reverse
+ *
+ * @s: string to be printed 0
+ * Return: void
+ *
+ */
+void _rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
