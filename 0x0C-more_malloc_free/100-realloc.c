f77cacc8d9e34c9bb0a026edad5a0c577dcc6350
Namsophia <namsophia9@gmail.com>
2023-06-15 15:16:23 -0700

    malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -24,7 +24,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		nptr = malloc(new_size);
-		if (nptr ==NULL)
+		if (nptr == NULL)
 			return (NULL);
 	}
 	if (new_size > old_size && (ptr != NULL))
