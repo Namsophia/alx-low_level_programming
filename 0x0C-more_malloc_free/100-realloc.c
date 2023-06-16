2bb7a6cc4b2ee59220d66779be35fa05aa2b366c
Namsophia <namsophia9@gmail.com>
2023-06-16 15:54:31 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,6 +9,7 @@
  * @new_size: size in bytes of new memory block
  * Return: pointer to new mem block, NULL or ptr
  */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *nptr;
