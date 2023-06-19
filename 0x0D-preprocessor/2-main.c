a0163568d6dd1e1bce05e5f3befe422f3e9105b4
Namsophia <namsophia9@gmail.com>
2023-06-19 13:56:57 -0700

    object like

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of the file program compiled
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,6 @@
+Preprocessor
+0. Object-like Macro
+1. Pi
+2. File name
+3. Function-like macro
+4. Sum
