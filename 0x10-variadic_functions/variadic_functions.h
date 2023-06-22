056ccec187c91849f882b088d368bc097b103f21
Namsophia <namsophia9@gmail.com>
2023-06-22 10:14:46 -0700

    variadic

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef _variadic_h
+#define _variaic_h
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *seperator, const unsigned int n, ...);
+void print_strings(const char *seperator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif
+
