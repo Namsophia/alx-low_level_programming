16836859b111634ac4e5544376bfe23cfa98885f
Namsophia <namsophia9@gmail.com>
2023-07-03 06:26:01 -0700

    1-print

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * _rev_recursion - prints string in reverse
+ * _print_rev_recursion - prints string in reverse
  *
  * @s: string to be printed 0
  * Return: void
  *
  */
-void _rev_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		_rev_recursion(s + 1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
