4a55261d5bf10ffafed89a357f6c4765ec63fba2
Namsophia <namsophie9@gmail.com>
2023-08-30 08:59:54 -0700

    pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,9 +21,8 @@ int (*get_op_func(char *s))(int, int)
 
 	int i = 0;
 
-	while (ops[i].op != NULL && *(op[i].op) != *s)
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
 
 	return (ops[i].f);
 }
-
