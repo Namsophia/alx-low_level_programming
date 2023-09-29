b63bc21b9cb486d9326a6db591628b3bf334a609
Namsophia <122439927+Namsophia@users.noreply.github.com>
2023-09-29 22:16:59 +0200

    Delete 0x10-variadic_functions directory
    deleted

--- 0x10-variadic_functions/variadic_functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef _variadic_h
-#define _variadic_h
-
-int _putchar(char c);
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *seperator, const unsigned int n, ...);
-void print_strings(const char *seperator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
-#endif
